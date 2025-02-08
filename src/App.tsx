117252e089b05e15c8e891ca89d24563cd8c6d57
Flashl3opard <sheoreyyash@gmail.com>
2025-02-08 23:35:41 +0530

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Navbar from "../public/components/Navbar";
 import Apps from "../public/components/Apps";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route } from "react-router-dom";
+
 function App() {
   return (
     <>
       <Navbar />
-
+      <h1>Hello</h1>
       <Apps />
     </>
   );
